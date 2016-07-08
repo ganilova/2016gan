1ba42465237128b0aac4287dbc865a84cd3b8810
Татьяна <ganilovat@gmail.com>
2016-07-08 13:31:37 +0700

    Gjcnjtybt uhfabrf aeyrwbb X*X

--- task_dm2.py
+++ task_dm2.py
@@ -3,8 +3,12 @@ from time import*
 def f(x):
     return x*x
 
+drawman_csale = 20
+x=-5.0
 pen_down()
-for x in range(0, 11):
+to_point(x, f(x))
+while x<=5:
     to_point(x, f(x))
+    x+=0.1
 pen_up()
 sleep(5)
