10c62d582c253c8911c255750a2eabfa600456c8
Татьяна <ganilovat@gmail.com>
2016-07-14 22:05:21 +0700

    Игра с мячиками (Вариант 2) Сделан подсчет "кликнутых" шариков

--- catch_then_ball/ball.py
+++ catch_then_ball/ball.py
@@ -1,18 +1,22 @@
 from tkinter import*
 from random import choice, randint
-ball_count = 40
+ball_count = 20
 ball_min = 15
 ball_max = 40
 ball_color = ['green', 'lightgrey', 'red', 'yellow', 'blue', 'lightgreen', 'lightblue', '#AA00FF']
+global points
 def click_ball(event):
     """ удаление шарика по клику мышки
     подсчет удаленных шариков """
+    global points, label, root
     obj = canvas.find_closest(event.x, event.y)
     x1, y1, x2, y2 =canvas.coords(obj)
     if x1<event.x < x2 and y1<event.y< y2:
         canvas.delete(obj)
-        #FIXME нужно учесть объект в очках
+        points+=1
+        label['text']=points
         create_random_ball()
+
 def move_all_balls(event):#Передвигает все шарики
     for obj in canvas.find_all():
         dx = randint(-5, 5)
@@ -37,8 +41,13 @@ def init_ball(): # Создает шарики для игры
         create_random_ball()
 
 def init_main_window():
-    global root, canvas
+    global root, canvas, label, points
     root = Tk()
+    label_text = Label(root, text = 'Набранные очки')
+    label_text.pack()
+    points = 0
+    label = Label(root, text=points)#привязка к переменной
+    label.pack()
     canvas = Canvas(root, background="white", width=400, height=400)
     canvas.bind("<Button>", click_ball)
     canvas.bind("<Motion>", move_all_balls)
