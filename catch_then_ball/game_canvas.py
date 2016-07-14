158a4004b6b61e1e3d31e5cb06bf45d23896c714
Татьяна <ganilovat@gmail.com>
2016-07-14 18:38:35 +0700

    Эксперименты с холстом

---
+++ catch_then_ball/game_canvas.py
@@ -0,0 +1,25 @@
+from tkinter import*
+
+
+def paint(event):
+    #print(event.x, event.y)
+    if event.widget != canvas:
+        print('Странно, ведь paint() привязали к canvas')
+        return
+    canvas.coords(line, 0, 0, event.x, event.y)
+
+
+
+root = Tk()
+
+canvas = Canvas(root, background="white", width=400, height=400)
+canvas.bind("<Motion>", paint)
+canvas.pack()
+
+line = canvas.create_line(0, 0, 10, 10)
+for i in range(10):
+    oval = canvas.create_oval(i*40, i*40, i*40+30, i*40+30, fill='lightgreen', outline='red')
+
+
+root.mainloop()
+print('Exit')
